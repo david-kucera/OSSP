31cb4c535f23e95c51a1d51094b6b34c463cb184
david-kucera <kucera.david2002@gmail.com>
2024-12-17 17:52:22 +0100

    Init repo

---
+++ OSSP/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace OSSP;
+
+/// <summary>
+/// Semestrálna práca z predmetu Optimalizácia sietí, FRI UNIZA, 2024
+/// Zadanie ZA - Duálna heuristika 2 - Simulated Annealing B
+/// Duálna heuristika: Algoritmus zväčšovania o najvýhodnejší uzol
+/// Simulated Annealing - Inverzia podreťazcov dĺžky 5
+/// SA parametre: tmax = 10_000, u = 40, q = 50
+/// </summary>
+class Program
+{
+    #region Constants
+    public static string ZA = "../data/ZA.txt";
+    #endregion // Constants
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Začiatok programu...");
+    }
+}
